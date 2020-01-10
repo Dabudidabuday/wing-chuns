1cee69cb3689e75f8e2782b40ee4bc81cdc5b1d1
Myroslav <94mir.ms@gmail.com>
2020-01-10 03:21:08 +0200

    first itterration

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// let { src, watch, dest, series } = require("gulp"),
+//     sass = require("sass"),
+//     del = retuire("del"),
+//     browserSync = require("browser-sync"),
+//     autoprefixer = require("gulp-autoprefixer"),
+//     gulpConcat = require("gulp-concat");
+
+// function style() {
+//   return src("app/scss/index.scss")
+//     .pipe(sass())
+//     .pipe(gulpConcat("main.css"))
+//     .pipe(dest("dist/"));
+// };
+
+// function startBrowserSync() {
+//   browserSync.init({
+//     server: {
+//       baseDir: "./"
+//     },
+//     notify: false
+//   });
+// };
+
+// function watcher() {
+//   startBrowserSync();
+//   watch("app/scss/**/*.scss").on("change", series(style, browserSync.reload));
+//   watch("index.html").on("change", browserSync.reload);
+// };
+
+// exports.run = watcher;
+
+const { src, watch, dest, series } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const gulpConcat = require("gulp-concat");
+
+function style() {
+  return src("app/scss/index.scss")
+    .pipe(sass())
+    .pipe(gulpConcat("main.css"))
+    .pipe(dest("dist/"));
+}
+
+function startBrowserSync() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    notify: false
+  });
+}
+
+function watcher() {
+  startBrowserSync();
+  watch("app/scss/**/*.scss").on("change", series(style, browserSync.reload));
+  watch("index.html").on("change", browserSync.reload);
+}
+
+exports.run = watcher;
