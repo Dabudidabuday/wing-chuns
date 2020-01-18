bcdab22b88e9f987220b3bf9bb3bee4d7e0a3088
roman bogdanov <cflatminor92@gmail.com>
2020-01-18 23:56:06 +0200

    accordeon wip

--- app/js/faq.js
+++ app/js/faq.js
@@ -3,26 +3,49 @@
     {
       button: document.querySelector('#question1 button'),
       answer: document.getElementById('answer1'),
+      icon: document.getElementById('faq-icon1'),
     },
     {
       button: document.querySelector('#question2 button'),
       answer: document.getElementById('answer2'),
+      icon: document.getElementById('faq-icon2'),
     },
     {
       button: document.querySelector('#question3 button'),
       answer: document.getElementById('answer3'),
+      icon: document.getElementById('faq-icon3'),
     }
   ];
 
-  function toggleAccordion(emitter, content) {
+  /**
+   * @method timedOutHtmlChange
+   * @param element {HTMLElement}
+   * @param value {String}
+   * @param delay {Number}
+   */
+  function timedOutHtmlChange (element, value, delay) {
+    setTimeout(function () {
+      element.innerHTML = value;
+    }, delay);
+  }
+
+  /**
+   * @method toggleAccordion {String}
+   * @param emitter {HTMLElement}
+   * @param content {HTMLElement}
+   * @param icon {HTMLElement}
+   */
+  function toggleAccordion(emitter, content, icon) {
     $(emitter).click(function () {
       if (content.style.maxHeight === '0px') {
+        timedOutHtmlChange(icon, `-`, 300);
         return content.style.maxHeight = '250px';
       }
 
+      timedOutHtmlChange(icon, `+`, 100);
       return content.style.maxHeight = '0px';
     });
   }
 
-  questions.forEach(item => toggleAccordion(item.button, item.answer));
+  questions.forEach(item => toggleAccordion(item.button, item.answer, item.icon));
 })();
